e7cd97bcb80b8938f71d5f27bbbe15d024850dda
ankang1 <ankang@excelsia.io>
2019-12-24 10:36:19 +0000

    delete unused codes

--- primer-android/src/net/bither/PrimerApplication.java
+++ primer-android/src/net/bither/PrimerApplication.java
@@ -44,9 +44,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
@@ -98,28 +95,6 @@ public class PrimerApplication extends Application {
         activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
 
         upgrade();
-        closeAndroidPDialog();
-    }
-
-    private void closeAndroidPDialog() {
-        try {
-            Class aClass = Class.forName("android.content.pm.PackageParser$Package");
-            Constructor declaredConstructor = aClass.getDeclaredConstructor(String.class);
-            declaredConstructor.setAccessible(true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            Class cls = Class.forName("android.app.ActivityThread");
-            Method declaredMethod = cls.getDeclaredMethod("currentActivityThread");
-            declaredMethod.setAccessible(true);
-            Object activityThread = declaredMethod.invoke(null);
-            Field mHiddenApiWarningShown = cls.getDeclaredField("mHiddenApiWarningShown");
-            mHiddenApiWarningShown.setAccessible(true);
-            mHiddenApiWarningShown.setBoolean(activityThread, true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
