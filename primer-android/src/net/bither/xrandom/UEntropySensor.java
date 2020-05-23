76fccafcbbbe727faac41f705784166a1d048357
ankang1 <ankang1@v.systems>
2020-05-23 20:07:20 +0100

    sensor frequency

--- primer-android/src/net/bither/xrandom/UEntropySensor.java
+++ primer-android/src/net/bither/xrandom/UEntropySensor.java
@@ -41,7 +41,7 @@ public class UEntropySensor implements SensorEventListener, IUEntropySource {
     private SensorManager sensorManager;
     private List<Sensor> sensors;
     private SensorVisualizerView visualizer;
-    private static final double ACCEPTANCEPROBABILITY = 0.01;
+    private static final double ACCEPTANCEPROBABILITY = 0.05;
 
     private boolean paused = true;
 
