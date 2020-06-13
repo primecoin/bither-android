28a29a67d0fd2696df8407d6816529bd9f30bd43
ankang1 <ankang1@v.systems>
2020-06-13 20:55:38 +0100

    update trending chart every 1 hour

--- primer-android/src/net/bither/util/PrimerTimer.java
+++ primer-android/src/net/bither/util/PrimerTimer.java
@@ -54,7 +54,7 @@ public class PrimerTimer {
                     while (!isStop) {
                         getExchangeTicker();
                         try {
-                            Thread.sleep(1 * 60 * 1000);
+                            Thread.sleep(60 * 60 * 1000);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
